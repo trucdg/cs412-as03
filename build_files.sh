98688802212d654af8c9a40ea576357d2a34777a
Truc Duong <trucdg@bu.edu>
2024-09-23 01:27:48 -0400

    fix python version

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,2 @@
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
